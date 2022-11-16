bd081e7640abef586e49a66f00f20993b94bfefd
fangjiwei <fangjiwei6354_xm.cicdi@chinaccs.cn>
2022-11-17 00:21:13 +0800

    11111

--- src/pages/MainLogin/index.tsx
+++ src/pages/MainLogin/index.tsx
@@ -2,7 +2,7 @@
  * @Author: fangjiwei fangjiwei6354_xm.cicdi@chinaccs.cn
  * @Date: 2022-11-15 16:00:16
  * @LastEditors: fangjiwei fangjiwei6354_xm.cicdi@chinaccs.cn
- * @LastEditTime: 2022-11-17 00:15:24
+ * @LastEditTime: 2022-11-17 00:19:48
  * @FilePath: \bugfixer\src\pages\MainLogin\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -66,10 +66,7 @@ const MainLogin: React.FC = () => {
                                         method:"post"
                                     }).then(res=>{
                                         console.log(res);
-                                        
                                     })
-                                    
-
                                 }} type="primary" size='large'>登录</Button>
                                 <div className='noPhoneContainer'>
                                     <div className='nophoneText'>没有账号？</div>
