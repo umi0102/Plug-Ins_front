ecb1789a837dbafc023d1b93363d08ad658d247e
fangjiwei <fangjiwei6354_xm.cicdi@chinaccs.cn>
2022-11-20 11:50:03 +0800

    添加单点登录，修改样式

--- src/pages/MainLogin/index.tsx
+++ src/pages/MainLogin/index.tsx
@@ -8,32 +8,18 @@
  */
 import React, { useEffect, useRef, useState } from 'react';
 import "./index.css";
-import { Button, Input, Tabs } from 'antd';
-import { requestjson } from '../../utils/request';
+import { Button, Input, Modal, Tabs } from 'antd';
 import { LoginByCode, LoginByForget, LoginByPhonePwd, SendCode } from './service';
-import Modal from 'antd/lib/modal/Modal';
-import { message, Space } from 'antd';
+import { message } from 'antd';
 
 const MainLogin: React.FC = () => {
 
-    const [PageState, setPageState] = useState<any>({
-        AllowClick: false,
-        PhoneCheck: false,
-        PwdCheck: false,
-        LoginType: "账号登陆",
-        UserPhone: "",
-        UserPwd: "",
-        Code:""
-    })
+
 
     const timerCount = 60
     const [count, setCount] = useState(timerCount)
     const timerRef = useRef(null)
-    //控制弹出层
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const showModal = () => {
-        setIsModalOpen(true);
-    };
+
     const cutCount = () => {
         setCount((prevState) => prevState - 1)
     }
@@ -41,9 +27,9 @@ const MainLogin: React.FC = () => {
         //发送验证码
         SendCode(PageState.UserPhone).then((res: any) => {
             if (res.code == "FP00000") {
-                CodeSuccess()
-            }else{
-                CodeError()
+                success("验证码发送成功")
+            } else {
+                error("验证码发送失败，请联系管理员！")
             }
         })
         cutCount()
@@ -60,53 +46,38 @@ const MainLogin: React.FC = () => {
 
     const [messageApi, contextHolder] = message.useMessage();
 
-    const success = () => {
+    const success = (messageStr: any) => {
         messageApi.open({
             type: 'success',
-            content: '登陆成功',
+            content: messageStr,
         });
     };
-    const CodeSuccess = () => {
+    const error = (messageStr: any) => {
         messageApi.open({
-            type: 'success',
-            content: '验证码发送成功',
+            type: 'error',
+            content: messageStr,
         });
     };
 
-    const error = () => {
-        messageApi.open({
-            type: 'error',
-            content: '用户名或密码不正确',
-        });
-    };
 
-    const CodeError = () => {
-        messageApi.open({
-            type: 'error',
-            content: '验证码发送失败，请联系管理员',
-        });
-    };
 
-    const warning = () => {
-        messageApi.open({
-            type: 'warning',
-            content: '请输入正确的用户名或密码',
-        });
-    };
-    
-    const codeCheckerror = () => {
-        messageApi.open({
-            type: 'error',
-            content: '验证码错误',
-        });
-    };
+    const [PageState, setPageState] = useState<any>({
+        AllowClick: false,
+        PhoneCheck: false,
+        PwdCheck: false,
+        LoginType: "账号登陆",
+        UserPhone: "",
+        UserPwd: "",
+        Code: "",
+
+    })
     return (
         <div className='container'>
-            <img className='back' src={require("./static/back.png")}></img>
+            <img className='back' src={require("./static/back.png")} alt="err"></img>
             <div className='content'>
                 <div className='contentItems'>
                     <div className='logoNameContainer'>
-                        <img className='logoImg' src={require('./static/logo.png')}></img>
+                        <img className='logoImg' src={require('./static/logo.png')} alt="err"></img>
                         <div className='logoText'>协创</div>
                     </div>
                     <div className='centerContainer'>
@@ -114,54 +85,70 @@ const MainLogin: React.FC = () => {
                             <div className='leftItem'>设计， 1 + 1 {">"} 2！</div>
                             <div className='leftBottom'>高效设计联调平台，产品经理用 RP，UI设计师用 DT</div>
                         </div>
-                        <div className={PageState.LoginType == "忘记密码登陆" ?  "rightItemMax":"rightItem"}>
+
+                        <div className={PageState.LoginType == "忘记密码登陆" ? "rightItemMax" : PageState.LoginType == "注册登陆" ? "rightItemReg" : "rightItem"}>
                             <div className='loginType'>
                                 <div className='dlText'>
-                                    <h1 className='loginTextName'>登录</h1>
+                                    <h1 className='loginTextName'>{PageState.LoginType == "注册登陆" ? "注册" : "登陆"}</h1>
                                 </div>
-                                <Tabs className='loginTypeContainer' activeKey={PageState.LoginType == "账号登陆" ? "1" : "2"} onTabClick={(e) => {
-                                    setPageState(Number(e) == 1 ? { ...PageState, LoginType: "账号登陆" } : { ...PageState, LoginType: "验证码登陆" })
-                                }}>
-                                    <Tabs.TabPane className='pwdContainer' tab="密码登录" key="1">
-                                        <Input className='pwdItem' placeholder='邮箱/手机号' onChange={(e) => {
+
+                                <div className={PageState.LoginType == "注册登陆" ? "phoneCheckContainer" : "none"}>
+                                    <Input className='phoneCheck' placeholder='邮箱/手机号' autoComplete="new-password" onChange={(e) => {
+                                        if (!(/^1(3|4|5|6|7|8|9)\d{9}$/.test(e.target.value))) {
+                                            setPageState({ ...PageState, PhoneCheck: false })
+                                            return
+                                        }
+                                        setPageState({ ...PageState, UserPhone: e.target.value, PhoneCheck: true })
+                                    }}></Input>
+                                </div>
+
+                                <Tabs className={
+                                    PageState.LoginType == "账号登陆" ? 'loginTypeContainer' : (PageState.LoginType == "验证码登陆" ? 'loginTypeContainer' : (PageState.LoginType == "注册登陆" ? "none" : 'loginTypeContainer'))}
+                                    activeKey={PageState.LoginType == "账号登陆" ? "1" : "2"} onTabClick={(e) => {
+                                        setPageState(Number(e) == 1 ? { ...PageState, LoginType: "账号登陆" } : { ...PageState, LoginType: "验证码登陆" })
+                                    }}>
+
+                                    <Tabs.TabPane className='pwdContainer' tab="密码登陆" key="1">
+                                        <Input className='pwdItem' placeholder='邮箱/手机号' autoComplete="new-password" onChange={(e) => {
                                             if (!(/^1(3|4|5|6|7|8|9)\d{9}$/.test(e.target.value))) {
                                                 setPageState({ ...PageState, PhoneCheck: false })
                                                 return
                                             }
                                             setPageState({ ...PageState, UserPhone: e.target.value, PhoneCheck: true })
                                         }}></Input>
-                                        <Input className='pwdItem' type='password' placeholder='密码' suffix={<div onClick={() => {
+                                        <Input className='pwdItem' type='password' placeholder='密码' autoComplete="new-password" suffix={<div onClick={() => {
                                             setPageState({ ...PageState, LoginType: "忘记密码登陆" })
                                         }}>忘记密码</div>} onChange={(e) => {
                                             if (e.target.value.length < 7) {
-                                                setPageState({ ...PageState, PwdCheck: false})
+                                                setPageState({ ...PageState, PwdCheck: false })
                                                 return
                                             }
                                             setPageState({ ...PageState, PwdCheck: true, UserPwd: e.target.value })
 
                                         }}></Input>
                                     </Tabs.TabPane>
-                                    <Tabs.TabPane className='pwdContainer' tab="验证码登录" key="2">
-                                        <Input className='pwdItem' placeholder='邮箱/手机号' onChange={(e) => {
+
+                                    <Tabs.TabPane className='pwdContainer' tab="验证码登陆" key="2">
+                                        <Input className='pwdItem' placeholder='邮箱/手机号' autoComplete="new-password" onChange={(e) => {
                                             if (!(/^1(3|4|5|6|7|8|9)\d{9}$/.test(e.target.value))) {
                                                 setPageState({ ...PageState, PhoneCheck: false })
                                             } else {
                                                 setPageState({ ...PageState, UserPhone: e.target.value, PhoneCheck: true })
                                             }
                                         }}></Input>
-                                        <Input className='pwdItem' placeholder='验证码' onChange={(e)=>{
-                                            setPageState({ ...PageState, Code: e.target.value})
+                                        <Input className='pwdItem' placeholder='验证码' autoComplete="new-password" onChange={(e) => {
+                                            setPageState({ ...PageState, Code: e.target.value })
                                         }} suffix={<div onClick={() => {
                                             if (PageState.PhoneCheck) {
                                                 sendCode()
                                             }
                                             return
                                         }}>{count === timerCount ? "发送验证码" : `还剩${count}秒`}</div>}></Input>
-                                        <Input className={PageState.LoginType == "忘记密码登陆" ? 'pwdItem' : "none"} type='password' placeholder='输入新密码' onChange={(e) => {
+                                        <Input className={PageState.LoginType == "忘记密码登陆" ? 'pwdItem' : "none"} type='password' placeholder='输入新密码' autoComplete="new-password" onChange={(e) => {
                                             if (e.target.value.length < 7) {
                                                 setPageState({ ...PageState, PwdCheck: false, })
                                             } else {
-                                                setPageState({ ...PageState, PwdCheck: true,UserPwd: e.target.value})
+                                                setPageState({ ...PageState, PwdCheck: true, UserPwd: e.target.value })
                                             }
 
                                         }}></Input>
@@ -183,30 +170,38 @@ const MainLogin: React.FC = () => {
                                                     token: res.data
                                                 }
                                                 window.localStorage.setItem("userGlobalData", JSON.stringify(userGlobalData))
-                                                return success()
+                                                return success("登陆成功")
                                             }
-                                            error()
+                                            error("用户名或密码错误")
 
                                         })
-                                    } else if (PageState.LoginType  == "验证码登陆"&& PageState.PhoneCheck ) {
+                                    } else if (PageState.LoginType == "验证码登陆" && PageState.PhoneCheck) {
                                         console.log("yzm");
                                         LoginByCode(PageState.UserPhone, PageState.Code).then((res: any) => {
                                             if (res.code == 200) {
-                                                return success()
+                                                return success("登陆成功")
                                             }
-                                            codeCheckerror()
+                                            error("验证码错误")
                                         })
-                                    } else if (PageState.LoginType  == "忘记密码登陆"&& PageState.PhoneCheck&& PageState.PwdCheck) {
+                                    } else if (PageState.LoginType == "忘记密码登陆" && PageState.PhoneCheck && PageState.PwdCheck) {
                                         console.log("wjmm");
                                         LoginByForget(PageState.UserPhone, PageState.Code, PageState.UserPwd)
                                     } else {
-                                        return warning()
+                                        return
                                     }
-                                }} type="primary" size='large'>登录</Button>
+                                }} type="primary" size='large'>登陆</Button>
                                 <div className='noPhoneContainer'>
-                                    <div className='nophoneText'>没有账号？</div>
-                                    <div className='loginText' onClick={showModal}>立即注册</div>
+                                    <div className='nophoneText'>{PageState.LoginType == "注册登陆" ? "已有账号" : "没有账号?"}</div>
+                                    <div className='loginText' onClick={() => {
+                                        if (PageState.LoginType == "注册登陆") {
+                                            return setPageState({ ...PageState, LoginType: "账号登陆" })
+                                        }
+                                        setPageState({ ...PageState, LoginType: "注册登陆" })
+                                    }}>{PageState.LoginType == "注册登陆" ? "立即登陆" : "立即注册"}</div>
+
                                 </div>
+
+
                             </div>
                         </div>
                     </div>
